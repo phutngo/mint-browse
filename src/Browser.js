6ec5fd4b441641418778ef6940dcc54c75ccd920
Phu Ngo <ptn123@gmail.com>
2021-09-13 23:41:19 -0500

    got token metadata

--- src/Browser.js
+++ src/Browser.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { NFTE } from "@nfte/react";
-import { Box } from "@material-ui/core";
+import { Box, Typography } from "@material-ui/core";
 import { getTokenMetadata } from "./utils/getTokenMetadata";
 import { useEffect, useState } from "react";
 import { Paper } from "@material-ui/core";
@@ -18,10 +18,19 @@ export const Browser = () => {
 
   return (
     <>
-      <Box style={{ display: "flex", justifyContent: "center" }} m={1}>
-        <Paper> {JSON.stringify(tokenMetaData)} </Paper>
-        <NFTE contract='0xb932a70a57673d89f4acffbe830e8ed7f75fb9e0' tokenId='18552' />
-      </Box>
+      <Paper>
+        <Box style={{ display: "flex", justifyContent: "center" }} m={1}>
+          <Typography variant='h6'>
+            NFT's minted from: {tokenMetaData.tokenMetaData.name}
+            {" ("}
+            {tokenMetaData.tokenMetaData.symbol}
+            {")"}
+          </Typography>
+        </Box>
+        <Box style={{ display: "flex", justifyContent: "center" }} m={1}>
+          <NFTE contract='0xb932a70a57673d89f4acffbe830e8ed7f75fb9e0' tokenId='18552' />
+        </Box>
+      </Paper>
     </>
   );
 };
